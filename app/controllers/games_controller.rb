445518219b5b928dd5a58df246140bdae57c42a5
Wred38 <nik.javor7@gmail.com>
2024-10-04 10:53:23 +0200

    Add game creation form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,4 +6,24 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
   end
+
+  def new
+    @game = Game.new
+    @players = Player.all
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to @game
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:game_name, :p1, :p2, :p3, :p4)
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+  validates :game_name, presence: true
+  validates :p1, presence: true
+  validates :p2, presence: true
+  validates :p3, presence: true
+  validates :p4, presence: true
 end
