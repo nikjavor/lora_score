b1590fc7b453bdbe349e2162dd7ffc61c1fa2cbd
Wred38 <nik.javor7@gmail.com>
2024-10-04 13:05:48 +0200

    Add validation for uniquenes of players. Change player selecrors to dropdown menu

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,24 @@ class Game < ApplicationRecord
   validates :p2, presence: true
   validates :p3, presence: true
   validates :p4, presence: true
+
+  validate :check_different_players
+
+  def check_different_players
+    players = [p1, p2, p3, p4]
+    len = players.length
+    for i in 1..len do
+      for j in 1..len do
+        if i != j && players[i] == players[j]
+          err_s = "must be unique to others"
+          errors.add(:p1, err_s) if i = 1
+          errors.add(:p2, err_s) if i = 2
+          errors.add(:p3, err_s) if i = 3
+          errors.add(:p4, err_s) if i = 4
+          return
+        end
+      end
+    end
+  end
+
 end
