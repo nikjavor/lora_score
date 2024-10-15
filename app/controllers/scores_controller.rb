df3f242b2026427ac4fcea3594b48fabed81330b
Wred38 <nik.javor7@gmail.com>
2024-10-15 21:50:14 +0200

    Add score saving capability

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,12 @@
 class ScoresController < ApplicationController
+  def update
+    @score = Score.find(params[:id])
+
+    @score.update(score_params)
+  end
+
+  private
+    def score_params
+      params.require(:score).permit(:value_p1, :value_p2, :value_p3, :value_p4)
+    end
 end
